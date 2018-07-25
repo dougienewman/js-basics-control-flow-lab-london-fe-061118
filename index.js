877018a9a9601e912c0eb56a324c9f71a26dcf4c
Doug Newman <dougnewman@gmail.com>
2018-07-25 18:13:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,6 +35,9 @@ function scuberGreetingForFeet(someValue) {
     if (tipValue == "generous"){
         result = 'Thank you so much.';
     }
+    if (tipValue != "generous"){
+        result = 'Thank you.';
+    }   
     return result
   }
 
